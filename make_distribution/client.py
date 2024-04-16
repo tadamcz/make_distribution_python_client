d2519bebc4978d0e62769c65e569ac220d326e22
tadamcz <tadamczewskipublic@gmail.com>
2024-04-16 14:44:04 +0100

    use HTTPs

--- make_distribution/client.py
+++ make_distribution/client.py
@@ -3,7 +3,7 @@ import requests
 
 class APIClient:
     def __init__(self, token, version="v0"):
-        self.base_url = f"http://makedistribution.com/s/api/{version}"
+        self.base_url = f"https://makedistribution.com/s/api/{version}"
         self.session = requests.Session()
         self.session.headers.update({"Authorization": f"Token {token}"})
 
